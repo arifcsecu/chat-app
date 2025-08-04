e99388a170ba92cb7f30f8e881bb5d5612eb0df0
arifcsecu <arifcsecu16@gmail.com>
2025-08-04 16:42:30 +0600

    test commit

--- resources/views/livewire/chat.blade.php
+++ resources/views/livewire/chat.blade.php
@@ -1,7 +1,7 @@
 <div class="relative mb-6 w-full">
     <flux:heading size="xl" level="1">{{ __('Chat App') }}</flux:heading>
     <flux:subheading size="lg" class="mb-6">{{ __('Manage your profile and account settings') }}</flux:subheading>
-    <flux:separator variant="subtle" />
+    <flux:separator variant="subtle" /> 
 
 
     <div class="flex h-[550px] text-sm border rounded-xl shadow overflow-hidden bg-white">
