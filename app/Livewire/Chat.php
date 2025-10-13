a1ef61f26d4a09db82033947298d738a904374a2
arifcsecu <arifcsecu16@gmail.com>
2025-10-13 17:45:35 +0600

    selection added

--- app/Livewire/Chat.php
+++ app/Livewire/Chat.php
@@ -10,10 +10,17 @@ use Illuminate\Support\Facades\Auth;
 class Chat extends Component
 {
     public $users;
+    public $selectedUser;
 
     public function mount ()
     {
         $this->users = User::whereNot("id", Auth::id())->get();
+        $this->selectedUser = $this->users->first();
+    }
+
+    public function selectUser($id)
+    {
+        $this->selectedUser = User::find($id);
     }
     
     public function render()
