a84b1152491107cbda4b9d371e3d04b1e5eaf840
arifcsecu <arifcsecu16@gmail.com>
2025-10-14 17:41:23 +0600

    learning two way messaging

--- resources/views/livewire/chat.blade.php
+++ resources/views/livewire/chat.blade.php
@@ -1,11 +1,11 @@
 <div class="relative w-full flex flex-col h-full">
-    <div class="flex text-sm border rounded-xl shadow overflow-hidden bg-white grow flex-1">
+    <div class="flex text-sm border rounded-xl shadow overflow-hidden bg-white flex-grow">
         <div class="w-2/12 border-r bg-gray-50">
             <div class="p-4 font-bold text-gray-700 border-b">Users</div>
 
             <div class="divide-y">
                 @foreach ($users as $user)
-                <div wire:click="selectUser({{ $user->id }})" class="p-3 cursor-pointer hover:bg-blue-100 transition flex justify-between items-center">
+                <div wire:click="selectUser({{ $user->id }})" class="p-3 cursor-pointer hover:bg-blue-100 transition flex justify-between items-center {{ $selectedUser->id === $user->id ? 'bg-blue-200 font-semibold' : '' }}">
                     <div>
                         <div class="text-gray-800">{{$user->name}}</div>
                         <div class="text-xs text-gray-500">{{ $user->email}}</div>
@@ -20,18 +20,20 @@
 
         <div class="w-8/12 flex flex-col">
             <div  class="p-4 border-b bg-gray-50">
-                <div class="text-lg font-semibold text-gray-800">{{$selectedUser->name}}</div>
-                <div class="text-xs text-gray-500">{{$selectedUser->email}}</div>
+                <div class="text-lg font-semibold text-gray-800">{{ $selectedUser->name }}</div>
+                <div class="text-xs text-gray-500">{{ $selectedUser->email }}</div>
             </div>
 
             <div class="flex-1 p-4 overflow-y-auto space-y-2 bg-gray-50">
-                <div class="flex justify-end">
-                    <div class="max-w-xs px-4 py-2 rounded-2xl shadow bg-blue-600 text-white">Hi, This is test.</div>
+                @foreach ($messages as $message)
+                <div class="flex justify-end {{ $message->sender_id === auth()->id() ? 'justify-end' : 'justify-start'}}">
+                    <div class="max-w-xs px-4 py-2 rounded-2xl shadow bg-blue-600 text-white">{{$message->message}}</div>
                 </div>
+                @endforeach
             </div>
 
-            <form class="p-4 border-t bg-white flex items-center gap-2">
-                <input type="text" class="flex-1 border border-gray-300 rounded-lg px-4 py-2 text-sm focus:outline"
+            <form wire:submit="submit" class="p-4 border-t bg-white flex items-center gap-2">
+                <input wire:model="newMessage" type="text" class="flex-1 border border-gray-300 rounded-lg px-4 py-2 text-sm focus:outline"
                     placeholder="Type your message...">
                 <button class="bg-blue-600 hover:bg-blue-700 text-white text-sm px-4 py-2 rounded-lg">Send</button>
             </form>
@@ -41,7 +43,7 @@
             <div class="w-full flex justify-center mt-2">
                 <div class="flex-col flex items-center gap-2">
                     <flux:avatar size="lg" src="https://unavatar.io/x/calebporzio" circle />
-                    <flux:heading size="lg" class="font-semibold">Ariful Islam</flux:heading>
+                    <flux:heading size="lg" class="font-semibold"> Ariful Islam </flux:heading>
                 </div>
             </div>
             
